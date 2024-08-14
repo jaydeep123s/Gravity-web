95bc878aff603405b0b23bf520acca7dd8aefeb5
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-14 16:44:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "Running tests..."
 cd app
 
 # Run a basic test to check if the app starts
-python -m unittest discover -s tests
+python3 -m unittest discover -s tests
 
 echo "Tests completed."
