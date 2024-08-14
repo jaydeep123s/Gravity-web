972b8b4803a74e4eb46cfdadff6595a553f5d059
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-14 16:56:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Create a virtual environment
+# Create a Python virtual environment
 python3 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# Install the required packages
+# Install required Python packages
 pip install -r requirements.txt
 
-# Run the tests
+# Run unit tests
 python -m unittest discover -s app/tests
 
 # Deactivate the virtual environment
