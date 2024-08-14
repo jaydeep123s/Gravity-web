d857f75ab484298358044321c5946c859ebd5d3d
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-14 17:35:24 +0530

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,15 +1,15 @@
 import unittest
-from app import app
+from app.app import app
 
 class BasicTests(unittest.TestCase):
-
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
     def test_home(self):
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
+        response = self.app.get('/')
+        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 200)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
