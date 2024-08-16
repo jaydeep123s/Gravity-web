e6d243dafb1409553c5709ea3b92b210a8e92364
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-16 14:34:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-# Print a message indicating the start of the deployment
-echo "Starting deployment..."
-
-# Navigate to the application directory or create it if it doesn't exist
-if [ ! -d "/home/ubuntu/deploy" ]; then
-    mkdir -p /home/ubuntu/deploy
-fi
-
-cd /home/ubuntu/deploy
-
-# Proceed with your existing deployment steps
